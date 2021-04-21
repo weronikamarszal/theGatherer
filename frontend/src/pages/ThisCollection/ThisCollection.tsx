119df9b28a1772d18872aa619c6113a5f796ae36
Danylo Arbuzov <arbuzdan@fit.cvut.cz>
2021-04-21 15:31:22 +0200

    this collection items

--- frontend/src/pages/ThisCollection/ThisCollection.tsx
+++ frontend/src/pages/ThisCollection/ThisCollection.tsx
@@ -8,8 +8,23 @@ class ThisCollection extends React.Component {
 
         return(
             <div>
-                <div className='thisCollectionTitle'>
-                    <h1>This collection</h1>
+                <div className='headWrapper'>
+                    <div className='thisCollectionTitle'>
+                        <h1>This collection</h1>
+                    </div>
+                    <div className='addButtonWrapper'>
+                        <button className='addCollectionButton'>Add Item</button>
+                    </div>
+                </div>
+                <hr className='horizontalLine'/>
+                <div className="dropdown">
+                    <button className="dropbtn">Sort by...</button>
+                    <div className="dropdown-content">
+                        <button className='sortButton'>Dimensions</button>
+                        <button className='sortButton'>Material</button>
+                        <button className='sortButton'>Date</button>
+                        <button className='sortButton'>Standard</button>
+                    </div>
                 </div>
                 <div className='itemsList'>
                     <div className='item'>
@@ -36,7 +51,13 @@ class ThisCollection extends React.Component {
                         <button className='viewItem'>View</button>
                     </div>
 
-
+                    {/*<div className='item'>*/}
+                    {/*    <img className='itemImage' src={itemImage} alt="itemImage"/>*/}
+                    {/*    <div className='itemDescription'>*/}
+                    {/*        Old stamp*/}
+                    {/*    </div>*/}
+                    {/*    <button className='viewItem'>View</button>*/}
+                    {/*</div>*/}
 
                 </div>
             </div>
