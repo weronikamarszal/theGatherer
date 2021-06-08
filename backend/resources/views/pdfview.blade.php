9b433a405787341a0c251b23aea728719c07f296
arbuzdan <danilaarbuzov0308@gmail.com>
2021-06-09 00:09:18 +0200

    pdf fixing

--- backend/resources/views/pdfview.blade.php
+++ backend/resources/views/pdfview.blade.php
@@ -9,6 +9,24 @@
         .itemName {
             text-align: center;
         }
+        .image {
+            max-width: 100%;
+        }
+        .itemImage {
+            margin-bottom: 30px;
+        }
+        .attributes {
+            background-color: #00DBDE;
+            background-image: linear-gradient(90deg, #00DBDE 0%, #FC00FF 100%);
+            height: auto;
+        }
+        .itemAttribute {
+            background-color: #F1FFFF;
+            width: 90%;
+            margin: 0 auto;
+            padding-left: 20px;
+        }
+
     </style>
 </head>
 <body>
@@ -16,12 +34,14 @@
     <h1>{{ $object->name }}</h1>
 </div>
 <div class="itemImage">
-    <img src="http://localhost:3000"{{$object->photo_path}}>
+    <img class="image" src="data:image/png;base64,{{ base64_encode(file_get_contents(public_path($object->photo_path))) }}">
 </div>
-@foreach($attributes as $key => $attribute)
-    <div class="itemAttribute">
-        <p>{{ $attribute->label }} : {{ $attributesValues[$key] }}</p>
-    </div>
-@endforeach
+<div class="attributes">
+    @foreach($attributes as $key => $attribute)
+        <div class="itemAttribute">
+            <p>{{ $attribute->label }} : {{ $attributesValues[$key] }}</p>
+        </div>
+    @endforeach
+</div>
 </body>
 </html>
